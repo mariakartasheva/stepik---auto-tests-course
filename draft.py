cab84a52c6886bfd72577f75acad3ac2a7f1f7a6
mighty_maria <m.bezymyannaya.com>
2019-10-05 20:58:36 -0400

    Played some more in the draft.

--- draft.py
+++ draft.py
@@ -1,15 +1,19 @@
-def name_length(name):
-    if len(name) > 0:
-        return str(len(name))
-    print("wtf") 
-    return ''
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result,\
+        f"Wrong answer: got {actual_result} instead of {expected_result}"
 
-def my_name(name, job):
-    print("Hello, my name is " + name + ". I am a " + job + ". My name's length is " + name_length(name))
+# def name_length(name):
+#     if len(name) > 0:
+#         return str(len(name))
+#     print("wtf") 
+#     return ''
 
-my_name('Dima', 'developer')
-my_name('Ira', 'doctor')
-my_name('Dasha', 'cook')
-my_name('', '?')
+# def my_name(name, job):
+#     print("Hello, my name is " + name + ". I am a " + job + ". My name's length is " + name_length(name))
+
+# my_name('Dima', 'developer')
+# my_name('Ira', 'doctor')
+# my_name('Dasha', 'cook')
+# my_name('', '?')
 
 # Играю с гитом :D
